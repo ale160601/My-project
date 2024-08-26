2cae32816cb1d8420391804aadeaa20b66c973a2
TheGalaxyBoyX <villegasfuentesf@gmail.com>
2024-08-26 01:27:27 -0400

    1.2 Menu Creado

---
+++ Assets/Scrips/Menu.cs
@@ -0,0 +1,37 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+using UnityEngine.UI;
+using UnityEngine.Audio;
+
+public class Menu : MonoBehaviour
+{
+    public AudioSource SFXSource;
+    public AudioClip SFXClip;
+    public Slider masterSlider, bgmSlider, sfxSlider;
+    public AudioMixer mixer;
+
+    private void Update()
+    {
+        mixer.SetFloat("MasterVolume", masterSlider.value);
+        mixer.SetFloat("BGMVolume", bgmSlider.value);
+        mixer.SetFloat("SFXVolume", sfxSlider.value);
+    }
+    public void PlayGame()
+    {
+        SceneManager.LoadScene("SampleScene");
+    }
+
+    public void PlaySound()
+    {
+        SFXSource.clip = SFXClip;
+        SFXSource.Play();
+    }
+
+    public void ExitGame()
+    {
+        Application.Quit();
+        Debug.Log("Saliste :)");
+    }
+}
